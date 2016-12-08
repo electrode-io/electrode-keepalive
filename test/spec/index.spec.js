25dc754fa87ff764f2310e0b1fb176ac629407f4
Jason Wilson <jrwilson@gmail.com>
2016-12-08 15:18:55 -0800

    dnsCache lookup and tests

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -1,9 +1,27 @@
 "use strict";
 
-const agent = require("../../lib/index");
+const httpAgent = require("../../lib/index");
 
 describe("index", () => {
   it("should export a function", () => {
-    expect(agent).to.be.a("function");
+    expect(httpAgent).to.be.a("function");
+  });
+
+  describe("httpAgent", () => {
+    it("should provide a preLookup function", () => {
+      const agent = httpAgent({});
+      expect(agent.preLookup).to.be.a("function");
+    });
+
+    it("should lookup hosts and populate dnsCache", (done) => {
+      const agent = httpAgent({});
+
+      agent.preLookup("www.google.com", (err, address) => {
+        expect(err).to.be.null;
+        expect(address).to.exist;
+        expect(agent.dnsCache["www.google.com"]).to.equal(address);
+        done();
+      });
+    });
   });
 });
